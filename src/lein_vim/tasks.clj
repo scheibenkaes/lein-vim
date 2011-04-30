58ea27e78671eb2b9867729c09b555138e7eaedc
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-04-30 12:35:36 +0200

    init

---
+++ src/lein_vim/tasks.clj
@@ -0,0 +1,7 @@
+(ns leiningen)
+
+(defn new-module
+  "Create a new clj file with the given name.
+  Also creates the corresponding test file.  "
+  [mod-name] )
+
---
+++ test/lein_vim/test/core.clj
@@ -0,0 +1,6 @@
+(ns lein-vim.test.core
+  (:use [lein-vim.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
