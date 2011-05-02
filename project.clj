0f2e16c29be6f643d6e63916ea4961cc79e05678
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-02 22:27:42 +0200

    Added :eval-in-leiningen true

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-vim "1.0.0-SNAPSHOT"
   :description "This project provides some leiningen tasks to be called from VIM"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
+  :eval-in-leiningen true)
