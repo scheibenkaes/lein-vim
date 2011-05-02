b576cc0da221caf04b5045437d0787f5639a9949
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-02 23:20:43 +0200

    lein now recognizes new_module

--- src/leiningen/new_module.clj
+++ src/leiningen/new_module.clj
@@ -1,10 +1,11 @@
 (ns leiningen.new-module
+  "Create a new clj file with the given name.
+  Also creates the corresponding test file.  "
   (:require [clojure.java.io :only (file)]))
 
 (defn new-module
   "Create a new clj file with the given name.
   Also creates the corresponding test file.  "
   [project mod-name]
-  (let [fname (to-file-name mod-name)]
-    (println project)))
+  (println project))
 
