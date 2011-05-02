cfe42fd8de919b4fb7fd766422801c27339e5059
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-03 00:40:36 +0200

    Writing a simple template to a file

--- src/leiningen/new_module.clj
+++ src/leiningen/new_module.clj
@@ -1,11 +1,20 @@
 (ns leiningen.new-module
   "Create a new clj file with the given name.
   Also creates the corresponding test file.  "
-  (:require [clojure.java.io :only (file)]))
+  (:use [clojure.java.io :only [file]]))
+
+(def src-template "(ns %s)\n")
+(def test-template "(ns %s)\n")
+
+(defn- create-src-module [path mod-name template] 
+  (let [package-expanded (.replace mod-name "." "/")
+        f (file (str path "/" package-expanded ".clj"))]
+    (when-not 
+      (.exists f)
+      (spit f template))))
 
 (defn new-module
   [project mod-name]
   (let [{:keys [source-path test-path]} project]
-    (create-src-module source-path mod-name)
-    (create-test-module test-path mod-name)))
+    (create-src-module source-path mod-name (format src-template mod-name))))
 
