918c5b0a7dea3b97ba67f96240646cda76848c90
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-22 16:28:28 +0200

    Made it version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-vim "1.0.0-SNAPSHOT"
+(defproject lein-vim "1.0.1-SNAPSHOT"
   :description "This project provides some leiningen tasks to be called from VIM"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
