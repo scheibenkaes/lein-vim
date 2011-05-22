2b53800c6469c93625fe1f54dad120679c17fb92
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-05-22 16:21:48 +0200

    Made new-module create the files

--- src/leiningen/new_module.clj
+++ src/leiningen/new_module.clj
@@ -4,7 +4,7 @@
   (:use [clojure.java.io :only [file]])
   (:require [clojure.string :as s]))
 
-(def src-template "(ns %s)\n")
+(def src-template "(ns %s.%s)\n")
 (def test-template "(ns %s.test.%s)\n")
 
 ; bar.foo => src/<proj-name>/foo/bar.clj test/<proj-name>/test/foo/bar.clj
@@ -28,26 +28,23 @@
 
 (defn- 
   create-file-with-template
-  "Create the file given by file-name and fills it with template.
+  "Create the file given by file and fills it with template.
   Only does this when there is not already a file with this name."
-  [file-name template] 
+  [file template] 
   (when-not 
-      (.exists file-name)
+      (.exists file)
       (do 
-        (.mkdirs (.getParentFile file-name))
-        (.createNewFile file-name)
-        (spit file-name template))))
+        (.mkdirs (.getParentFile file))
+        (.createNewFile file)
+        (spit file template))))
 
 (defn new-module
   [project mod-name]
-  (let [{:keys [source-path test-path]} project
+  (let [{:keys [source-path test-path name]} project
         files (create-file-names project mod-name)
-        src (format src-template mod-name)
-        test (format test-template (:name project) mod-name)]
+        src (format src-template name mod-name)
+        test (format test-template name mod-name)]
     (do
-      (create-file-with-template (:src files) src)
-      (create-file-with-template (:test files) test))
-    (println files)
-    (println src)
-    (println test)))
+      (create-file-with-template (file (:src files)) src)
+      (create-file-with-template (file (:test files)) test))))
 
